cc9f7fde42c33ff0e633572eff1d2151af905489
Talha Shahid Alvi <bsee18018@itu.edu.pk>
2022-07-14 17:26:30 +0500

    Updated version Of Movie Website

--- final/main.js
+++ final/main.js
@@ -42,3 +42,13 @@ var swiper = new Swiper(".trending-content", {
     },
   });
   
+
+// Custom Scroll bar 
+window.onscroll = function() {mufunction()};
+
+function mufunction(){
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height)*100;
+  document.getElementById('scroll-bar').style.width = scrolled + '%';
+}
